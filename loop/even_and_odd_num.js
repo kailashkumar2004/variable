ca5f77fb2bb1af9d8cee7a9b409cf1ac74c9faef
kailashkumar2004 <kailashkumartkg@gmail.com>
2023-05-14 14:38:41 +0530

    first commit

--- loop/even_and_odd_num.js
+++ loop/even_and_odd_num.js
@@ -127,5 +127,122 @@
 
 // var x = 2;
 // var y = "2";
-console.log(6<=6)  // Returns true since the value of both x and y is the same
+// console.log(6<=6)  // Returns true since the value of both x and y is the same
 // console.log(x === y)
+
+
+// var i=1
+// while(i<=20){
+//     if(i%2==0){
+//         console.log("even hai")
+//     }
+//     else{
+//         console.log("odd hai")
+//     }
+//     i=i+1
+// }
+
+// var rlSync =require("readline-sync")
+// var num =rlSync.questionInt("enter any num=====")
+// var i=1
+// while(i<=num){
+//     if(i%2==0){
+//         console.log(i,"even number")
+//     }
+//     else{
+//         console.log(i,"odd number")
+//     }
+//     i=i+1
+// }
+
+
+// OddNumber =0;
+// evenNumber =0;
+// number =1;
+// while(number<=20){
+//     if(number%2!=0){
+//         OddNumber=OddNumber+number
+//     }
+//     else{
+//         evenNumber=evenNumber+number
+//     }
+//     number=number++
+// }
+// console.log("total of oddnumber")
+// console.log("total of evennumber")
+
+// var rlSync =require("readline-sync")
+// var num =rlSync.questionInt("enter any num====")
+// var evenSum =0;
+// var oddSum =0;
+// var i=1
+// while(i<=num){
+//     if(i%2==0){
+//         console.log(i,"even")
+//         sum=sum+i
+//     }
+//     else if(i%2!=0){
+//         console.log(i,"odd")
+//         odd=odd+i
+//     }
+//     i=i+1
+// }
+
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter any num===")
+// var i=2
+// count =0
+// while(i<=num){
+//     if(num%i==0){
+//         count=count+1
+//     }
+//     i=i+1
+// }
+// console.log("count-----",count)
+// if(count==1){
+//     console.log("it is a prime number")
+// }
+// else{
+//     console.log("it is a not prime number")
+// }
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter any num====")
+// fact =1
+// while(num>0){
+//     fact=fact*num
+//     num=num-1
+// }
+// console.log("fact")
+
+// var readline =require("readline-sync")
+// var num =readline.questionInt("enter any num====")
+// var i=0
+// while(i<=num){
+//     var num1=readline.questionInt("enter any num1===")
+//     if(num>i){
+//         console.log(num1,"postive number")
+//     }
+//     else{
+//         console.log(num1,"neagtive number")
+//     }
+//     i=i+1
+// }
+
+var rlSync =require("readline-sync")
+var num =rlSync.questionInt("enter any num===")
+var i=0
+var sum=0
+while(i<num){
+    if(num%i==0){
+        sum=sum+i
+    }
+    i=i+1
+}
+if(sum==num){
+    console.log(sum,"it is a perfect number")
+}
+else{
+    console.log(sum,"it is not perfect number")
+}
